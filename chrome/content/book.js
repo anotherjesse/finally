73cfbdf6f50b46f1dbc56e463bf25fe58b025eda
Jesse Andrews <anotherjesse@gmail.com>
2008-02-14 02:40:17 -0800

    playing with a xbl for the "notes" (bad name)

--- chrome/content/book.js
+++ chrome/content/book.js
@@ -1,17 +1,40 @@
 var dnd= {
-	 onDragOver: function(event,flavor,session) {},
-	 onDrop: function(evt,dropData,asession) {},
-	 onDragStart: function(event, transferData, action) {},
+	 onDragOver: function(event,flavor,session) {
+	 },
+	 onDrop: function(event,dropData,asession) {
+	   try {
+  	   var note = document.createElement('note');
+  	   $('#container').append(note);
+       note.style.position = 'fixed';
+       note.style.left = event.clientX-12 + 'px';
+       note.style.top = event.clientY-12 + 'px';
+       console.log(dropData.flavour)
+       if (dropData.flavour.contentType == "text/html") {
+         note.data = {description: dropData.data};
+       }
+       if (dropData.flavour.contentType == "text/unicode") {
+         note.data = {description: dropData.data};
+       }
+       if (dropData.flavour.contentType == "text/x-moz-url") {
+         var bm = dropData.data.split("\n");
+         note.data = {name: bm[1], description: bm[0]};
+       }
+	   } catch(e) {
+	     console.log(e);
+	   }
+	 },
+	 onDragStart: function(event, transferData, action) {
+	 },
 	 getSupportedFlavours: function() {
 			var flavors = new FlavourSet();
-			flavors.appendFlavour('text/unicode');
-			flavors.appendFlavour('text/html');
 			flavors.appendFlavour('text/x-moz-url');
+			flavors.appendFlavour('text/html');
 			flavors.appendFlavour('application/x-moz-file','nsIFile');
+			flavors.appendFlavour('text/unicode');
 			return flavors;
 	 }
 }
 
 $(function() {
-  $('#status').attr('value', 'inited')
+  $('#map')[0].data = {name: 'a song', description: 'song description'}
 })
