bf97e8520fab08772c3f98ebd0967c325dcaf512
Jesse Andrews <anotherjesse@gmail.com>
2008-02-14 21:47:33 -0800

    dnd notes are stored in rdf, display is done via rdf
      * notes xbl initializes via a rdf resource id
      * dnd creates a node and sets properties
      * display is done via a xul template

--- chrome/content/book.js
+++ chrome/content/book.js
@@ -1,3 +1,30 @@
+var rdf = new (function() {
+  try {
+    const Cc = Components.classes;
+    const Ci = Components.interfaces;
+    const RDFS = Cc['@mozilla.org/rdf/rdf-service;1'].getService(Ci.nsIRDFService);
+    const RDFCU = Cc['@mozilla.org/rdf/container-utils;1'].getService(Ci.nsIRDFContainerUtils);
+    const NSRDF = function(name) { return RDFS.GetResource('http://home.netscape.com/NC-rdf#'+name); }
+
+    var ds = RDFS.GetDataSource('rdf:books', false);
+    console.log(ds)
+    var bag = RDFCU.MakeBag(ds, RDFS.GetResource('urn:root'));
+    console.log(bag)
+
+    this.add = function(data) {
+      var resource = RDFS.GetAnonymousResource();
+      for (var k in data) {
+        ds.Assert(resource, NSRDF(k), RDFS.GetLiteral(data[k]), true);
+      }
+      bag.AppendElement(resource);
+    }
+  }
+  catch (e) {
+    console.log('error', e)
+  }
+
+})();
+
 var dnd = {
   onDragOver: function(event,flavor,session) {},
   onDrop: function(event,dropData,asession) {
@@ -8,18 +35,16 @@ var dnd = {
       note.style.left = event.clientX-12 + 'px';
       note.style.top = event.clientY-12 + 'px';
       if (dropData.flavour.contentType == "text/html") {
-        data = {description: dropData.data};
+        data = {description: dropData.data, kind: 'html'};
       }
       if (dropData.flavour.contentType == "text/unicode") {
-        data = {description: dropData.data};
+        data = {description: dropData.data, kind: 'text'};
       }
       if (dropData.flavour.contentType == "text/x-moz-url") {
         var bm = dropData.data.split("\n");
-        data = {name: bm[1], description: bm[0]};
-        note.className = 'url'
+        data = {name: bm[1], description: bm[0], kind: 'link'};
       }
-      $('#container').append(note);
-      note.data = data;
+      rdf.add(data)
     } catch(e) {
       console.log(e);
     }
@@ -34,23 +59,3 @@ var dnd = {
     return flavors;
   }
 }
-
-$(function() {
-  try {
-    const Cc = Components.classes;
-    const Ci = Components.interfaces;
-    const RDFS = Cc['@mozilla.org/rdf/rdf-service;1'].getService(Ci.nsIRDFService);
-    const NSRDF = function(name) { return RDFS.GetResource('http://home.netscape.com/NC-rdf#'+name); }
-
-    var ds = RDFS.GetDataSource('rdf:books', false);
-
-    var resource = RDFS.GetResource('urn:test');
-    ds.Assert(resource, NSRDF('foo'), RDFS.GetLiteral('bar'), true);
-    console.log("asssertion test passes:", ds.HasAssertion(resource, NSRDF('foo'), RDFS.GetLiteral('bar'), true));
-    console.log("false asssertion test passes:", !ds.HasAssertion(resource, NSRDF('foo'), RDFS.GetLiteral('baz'), true));
-  } catch (e) {
-    console.log('erorr:', e)
-    alert(e)
-  }
-
-})
