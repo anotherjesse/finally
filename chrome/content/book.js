38144ad2345ef5a3d2a88f62e8fe2fdfedefdfc6
Jesse Andrews <anotherjesse@gmail.com>
2008-02-14 16:30:30 -0800

    add data afterward so it calls the setter

--- chrome/content/book.js
+++ chrome/content/book.js
@@ -1,41 +1,36 @@
-var dnd= {
-	 onDragOver: function(event,flavor,session) {
-	 },
-	 onDrop: function(event,dropData,asession) {
-	   try {
-  	   var note = document.createElement('note');
-       note.style.position = 'fixed';
-       note.style.left = event.clientX-12 + 'px';
-       note.style.top = event.clientY-12 + 'px';
-       console.log(dropData.flavour)
-       if (dropData.flavour.contentType == "text/html") {
-         note.data = {description: dropData.data};
-       }
-       if (dropData.flavour.contentType == "text/unicode") {
-         note.data = {description: dropData.data};
-       }
-       if (dropData.flavour.contentType == "text/x-moz-url") {
-         var bm = dropData.data.split("\n");
-         note.data = {name: bm[1], description: bm[0]};
-         note.className = 'url'
-       }
-     	 $('#container').append(note);
-	   } catch(e) {
-	     console.log(e);
-	   }
-	 },
-	 onDragStart: function(event, transferData, action) {
-	 },
-	 getSupportedFlavours: function() {
-			var flavors = new FlavourSet();
-			flavors.appendFlavour('text/x-moz-url');
-			flavors.appendFlavour('text/html');
-			// flavors.appendFlavour('application/x-moz-file','nsIFile');
-			flavors.appendFlavour('text/unicode');
-			return flavors;
-	 }
+var dnd = {
+  onDragOver: function(event,flavor,session) {},
+  onDrop: function(event,dropData,asession) {
+    try {
+      var note = document.createElement('note');
+      var data = {};
+      note.style.position = 'fixed';
+      note.style.left = event.clientX-12 + 'px';
+      note.style.top = event.clientY-12 + 'px';
+      if (dropData.flavour.contentType == "text/html") {
+        data = {description: dropData.data};
+      }
+      if (dropData.flavour.contentType == "text/unicode") {
+        data = {description: dropData.data};
+      }
+      if (dropData.flavour.contentType == "text/x-moz-url") {
+        var bm = dropData.data.split("\n");
+        data = {name: bm[1], description: bm[0]};
+        note.className = 'url'
+      }
+      $('#container').append(note);
+      note.data = data;
+    } catch(e) {
+      console.log(e);
+    }
+  },
+  onDragStart: function(event, transferData, action) {},
+  getSupportedFlavours: function() {
+    var flavors = new FlavourSet();
+    flavors.appendFlavour('text/x-moz-url');
+    flavors.appendFlavour('text/html');
+    // flavors.appendFlavour('application/x-moz-file','nsIFile');
+    flavors.appendFlavour('text/unicode');
+    return flavors;
+  }
 }
-
-$(function() {
-  $('#map')[0].data = {name: 'a song', description: 'song description'}
-})
